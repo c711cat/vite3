d6adfe1596ca4798d20d5eb9d06d2d1ee88530ae
c711cat <c711cat@gmail.com>
2024-10-20 15:08:24 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base:'vite3/'
+  base:'vite3/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
